72621af5591007137dad35085f0e2364c3f8e019
JacRoss <tori.belechy@gmail.com>
2018-08-08 12:50:02 +0300

    -

--- artisan.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-$application = new \Symfony\Component\Console\Application('Console App');
-$application->add(new \Jackross\SumCommand());
-$application->add(new \Jackross\SckobkiCommand());
-$application->run();
